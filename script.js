1c8f7da4d74f2cacbc2972a3f115f123e576c0ee
mourtashi <109982496+mourtashi@users.noreply.github.com>
2023-09-08 10:59:12 -0400

    Formatting response from API Call

--- script.js
+++ script.js
@@ -43,18 +43,31 @@ async function showMealPlan() {
       body: JSON.stringify(payload)
     });
 
-    // Process the response
-    if (response.ok) {
-      const jsonResponse = await response.json();
-      console.log("API response received:", jsonResponse);
+// Process the response
+if (response.ok) {
+  const jsonResponse = await response.json();
+  console.log("API response received:", jsonResponse);
 
-      const parsedMealPlan = JSON.parse(jsonResponse.body);  // Parsing the body to get the meal plan
-      document.getElementById('meal-plan').innerHTML = parsedMealPlan.meal_plan;
-      document.getElementById('meal-plan-container').style.display = 'block';
-    } else {
-      alert('Something went wrong!');
-    }
-  } catch (error) {
-    console.error('There was a problem with the fetch operation:', error);
-  }
+  const parsedMealPlan = JSON.parse(jsonResponse.body);  // Parsing the body to get the meal plan
+  const rawMealPlan = parsedMealPlan.meal_plan;
+  
+  // Formatting the meal plan
+  let formattedMealPlan = rawMealPlan
+    .replace("Breakfast:", "<strong>Breakfast:</strong><ul><li>")
+    .replace("Lunch:", "</li></ul><strong>Lunch:</strong><ul><li>")
+    .replace("Dinner:", "</li></ul><strong>Dinner:</strong><ul><li>")
+    .replace("Snack:", "</li></ul><strong>Snack:</strong><ul><li>");
+  
+  // Replace each food separator '-' with a new list item
+  formattedMealPlan = formattedMealPlan.replace(/-\s*(.+?)(?=-|$)/g, (_, item) => `<li>${item}</li>`);
+  
+  // Close the last unordered list
+  formattedMealPlan += "</ul>";
+
+  document.getElementById('meal-plan').innerHTML = formattedMealPlan;
+  document.getElementById('meal-plan-container').style.display = 'block';
+  
+} else {
+  alert('Something went wrong!');
 }
+
